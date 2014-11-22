f885151dd5d8ddacd26321e727b5402a90f58e4d
ndsrf <jgarciamagna@gmail.com>
2014-11-22 23:29:05 +0100

    Philips hue - simpler way of doing the PUT request

--- MainServer/MainServer/Infrastructure/HueLights/PhilipsHue.cs
+++ MainServer/MainServer/Infrastructure/HueLights/PhilipsHue.cs
@@ -8,56 +8,49 @@ using Nancy.Conventions;
 
 namespace MainServer.Infrastructure.HueLights
 {
-    class PhilipsHue
-    {
-        public static IPAddress HubIp;
-        public static string Key;
-        public static string SceneIn;
+	class PhilipsHue
+	{
+		public static IPAddress HubIp;
+		public static string Key;
+		public static string SceneIn;
 
-        public static void LightsOn()
-        {
-            SetLights(10);
-        }
+		public static void LightsOn()
+		{
+			SetLights(10);
+		}
 
-        public static void LightsOff()
-        {
-            SetLights(0);
-        }
+		public static void LightsOff()
+		{
+			SetLights(0);
+		}
 
-        private static void SetLights(int level)
-        {
-            string content;
-            if (level == 0)
-            {
-                content = @"
-                            {
-	                            ""on"": false
-                            }
+		private static void SetLights(int level)
+		{
+			string content;
+			if (level == 0)
+			{
+				content = @"
+							{
+								""on"": false
+							}
 
-                            ";
-            }
-            else
-            {
-                content = @"
-                            {
-	                            ""scene"": """ + SceneIn + @"""
-                            }
+							";
+			}
+			else
+			{
+				content = @"
+							{
+								""scene"": """ + SceneIn + @"""
+							}
 
-                            ";
-            }
+							";
+			}
 
-            var data = Encoding.Default.GetBytes(content);
+			var data = Encoding.Default.GetBytes(content);
 
-            var wc = WebRequest.Create("http://" + HubIp.ToString() + "/api/" + Key + "/groups/0/action");
-            ((HttpWebRequest) wc).UserAgent = "MainServer.RaspberryHome(0.1)";
-            wc.ContentType = "application/json";
-            wc.ContentLength = data.Length;
-            wc.Method = "PUT";
-
-            var stream = wc.GetRequestStream();
-            stream.Write(data, 0, data.Length);
-            stream.Close();
-            var webresponse = wc.GetResponse();
-        }
-    }
+		    var wc = new WebClient();
+		    var url = "http://" + HubIp + "/api/" + Key + "/groups/0/action";
+		    wc.UploadString(url, "PUT", content);
+		}
+	}
 }
